eaeb5216f40977129c7571fb9e463a47f7071cec
JosephWest2 <joeywest1414@gmail.com>
2024-12-09 14:16:08 -0700

    refactor wip

---
+++ src/renderer/resize_dependent_components/depth_image_components.rs
@@ -0,0 +1,124 @@
+use ash::vk;
+
+use crate::renderer::{find_memorytype_index, record_submit_commandbuffer};
+
+pub struct DepthImageComponents {
+    pub depth_image: vk::Image,
+    pub depth_image_view: vk::ImageView,
+    pub depth_image_memory: vk::DeviceMemory,
+}
+
+pub const DEPTH_IMAGE_FORMAT: vk::Format = vk::Format::D16_UNORM;
+
+impl DepthImageComponents {
+    pub fn new(
+        device: &ash::Device,
+        device_memory_properties: &vk::PhysicalDeviceMemoryProperties,
+        surface_resolution: &vk::Extent2D,
+        setup_command_buffer: &vk::CommandBuffer,
+        setup_commands_reuse_fence: &vk::Fence,
+        present_queue: &vk::Queue,
+    ) -> Self {
+        let depth_image_create_info = vk::ImageCreateInfo::default()
+            .image_type(vk::ImageType::TYPE_2D)
+            .format(DEPTH_IMAGE_FORMAT)
+            .extent((*surface_resolution).into())
+            .mip_levels(1)
+            .array_layers(1)
+            .samples(vk::SampleCountFlags::TYPE_1)
+            .tiling(vk::ImageTiling::OPTIMAL)
+            .usage(vk::ImageUsageFlags::DEPTH_STENCIL_ATTACHMENT)
+            .sharing_mode(vk::SharingMode::EXCLUSIVE);
+
+        let depth_image = unsafe { device.create_image(&depth_image_create_info, None).unwrap() };
+
+        let depth_image_memory_reqs = unsafe { device.get_image_memory_requirements(depth_image) };
+
+        let depth_image_memory_index = find_memorytype_index(
+            &depth_image_memory_reqs,
+            &device_memory_properties,
+            vk::MemoryPropertyFlags::DEVICE_LOCAL,
+        )
+        .expect("Cannot find suitable memory index for depth image");
+
+        let depth_image_allocate_info = vk::MemoryAllocateInfo::default()
+            .allocation_size(depth_image_memory_reqs.size)
+            .memory_type_index(depth_image_memory_index);
+
+        let depth_image_memory = unsafe {
+            device
+                .allocate_memory(&depth_image_allocate_info, None)
+                .unwrap()
+        };
+
+        record_submit_commandbuffer(
+            &device,
+            *setup_command_buffer,
+            *setup_commands_reuse_fence,
+            *present_queue,
+            &[],
+            &[],
+            &[],
+            |device, setup_command_buffer| {
+                let layout_transition_barrier = vk::ImageMemoryBarrier::default()
+                    .image(depth_image)
+                    .dst_access_mask(
+                        vk::AccessFlags::DEPTH_STENCIL_ATTACHMENT_READ
+                            | vk::AccessFlags::DEPTH_STENCIL_ATTACHMENT_WRITE,
+                    )
+                    .new_layout(vk::ImageLayout::DEPTH_STENCIL_ATTACHMENT_OPTIMAL)
+                    .old_layout(vk::ImageLayout::UNDEFINED)
+                    .subresource_range(
+                        vk::ImageSubresourceRange::default()
+                            .aspect_mask(vk::ImageAspectFlags::DEPTH)
+                            .layer_count(1)
+                            .level_count(1),
+                    );
+                unsafe {
+                    device.cmd_pipeline_barrier(
+                        setup_command_buffer,
+                        vk::PipelineStageFlags::BOTTOM_OF_PIPE,
+                        vk::PipelineStageFlags::LATE_FRAGMENT_TESTS,
+                        vk::DependencyFlags::empty(),
+                        &[],
+                        &[],
+                        &[layout_transition_barrier],
+                    )
+                };
+            },
+        );
+
+        let depth_image_view_info = vk::ImageViewCreateInfo::default()
+            .subresource_range(
+                vk::ImageSubresourceRange::default()
+                    .aspect_mask(vk::ImageAspectFlags::DEPTH)
+                    .level_count(1)
+                    .layer_count(1),
+            )
+            .image(depth_image)
+            .format(depth_image_create_info.format)
+            .view_type(vk::ImageViewType::TYPE_2D);
+
+        let depth_image_view = unsafe {
+            device
+                .create_image_view(&depth_image_view_info, None)
+                .unwrap()
+        };
+
+
+        Self {
+            depth_image,
+            depth_image_memory,
+            depth_image_view,
+        }
+    }
+}
+
+pub fn cleanup_depth_image_components(device: &ash::Device, depth_image_components: &DepthImageComponents) {
+    unsafe {
+        device.device_wait_idle().unwrap();
+        device.destroy_image_view(depth_image_components.depth_image_view, None);
+        device.destroy_image(depth_image_components.depth_image, None);
+        device.free_memory(depth_image_components.depth_image_memory, None);
+    }
+}
---
+++ src/renderer/resize_dependent_components/swapchain_components.rs
@@ -0,0 +1,140 @@
+use ash::{
+    khr::{surface, swapchain},
+    vk,
+};
+
+pub struct SwapchainComponents {
+    pub swapchain: vk::SwapchainKHR,
+    pub present_images: Vec<vk::Image>,
+    pub present_image_views: Vec<vk::ImageView>,
+    pub surface_format: vk::SurfaceFormatKHR,
+    pub surface_resolution: vk::Extent2D,
+}
+
+impl SwapchainComponents {
+    pub fn new(
+        device: &ash::Device,
+        window: &winit::window::Window,
+        surface: &vk::SurfaceKHR,
+        surface_loader: &surface::Instance,
+        swapchain_loader: &swapchain::Device,
+        physical_device: &vk::PhysicalDevice,
+    ) -> Self {
+        let surface_format = unsafe {
+            surface_loader
+                .get_physical_device_surface_formats(*physical_device, *surface)
+                .unwrap()[0]
+        };
+
+        let surface_capabilities = unsafe {
+            surface_loader
+                .get_physical_device_surface_capabilities(*physical_device, *surface)
+                .unwrap()
+        };
+
+        let mut desired_image_count = surface_capabilities.min_image_count + 1;
+
+        if surface_capabilities.max_image_count > 0
+            && desired_image_count > surface_capabilities.max_image_count
+        {
+            desired_image_count = surface_capabilities.max_image_count;
+        }
+
+        let surface_resolution = match surface_capabilities.current_extent.width {
+            u32::MAX => vk::Extent2D {
+                width: window.inner_size().width.max(1),
+                height: window.inner_size().height.max(1),
+            },
+            _ => surface_capabilities.current_extent,
+        };
+
+        let pre_transform = if surface_capabilities
+            .supported_transforms
+            .contains(vk::SurfaceTransformFlagsKHR::IDENTITY)
+        {
+            vk::SurfaceTransformFlagsKHR::IDENTITY
+        } else {
+            surface_capabilities.current_transform
+        };
+
+        let present_modes = unsafe {
+            surface_loader
+                .get_physical_device_surface_present_modes(*physical_device, *surface)
+                .unwrap()
+        };
+
+        let present_mode = present_modes
+            .iter()
+            .cloned()
+            .find(|&mode| mode == vk::PresentModeKHR::MAILBOX)
+            .unwrap_or(vk::PresentModeKHR::FIFO);
+
+        let swapchain_create_info = vk::SwapchainCreateInfoKHR::default()
+            .surface(*surface)
+            .min_image_count(desired_image_count)
+            .image_color_space(surface_format.color_space)
+            .image_format(surface_format.format)
+            .image_extent(surface_resolution)
+            .image_usage(vk::ImageUsageFlags::COLOR_ATTACHMENT)
+            .image_sharing_mode(vk::SharingMode::EXCLUSIVE)
+            .pre_transform(pre_transform)
+            .composite_alpha(vk::CompositeAlphaFlagsKHR::OPAQUE)
+            .present_mode(present_mode)
+            .clipped(true)
+            .image_array_layers(1);
+
+        let swapchain = unsafe {
+            swapchain_loader
+                .create_swapchain(&swapchain_create_info, None)
+                .unwrap()
+        };
+
+        let present_images = unsafe { swapchain_loader.get_swapchain_images(swapchain).unwrap() };
+
+        let present_image_views: Vec<vk::ImageView> = present_images
+            .iter()
+            .map(|&image| {
+                let create_view_info = vk::ImageViewCreateInfo::default()
+                    .view_type(vk::ImageViewType::TYPE_2D)
+                    .format(surface_format.format)
+                    .components(vk::ComponentMapping {
+                        r: vk::ComponentSwizzle::R,
+                        g: vk::ComponentSwizzle::G,
+                        b: vk::ComponentSwizzle::B,
+                        a: vk::ComponentSwizzle::A,
+                    })
+                    .subresource_range(vk::ImageSubresourceRange {
+                        aspect_mask: vk::ImageAspectFlags::COLOR,
+                        base_mip_level: 0,
+                        level_count: 1,
+                        base_array_layer: 0,
+                        layer_count: 1,
+                    })
+                    .image(image);
+                unsafe { device.create_image_view(&create_view_info, None).unwrap() }
+            })
+            .collect();
+
+        SwapchainComponents {
+            swapchain,
+            present_image_views,
+            present_images,
+            surface_resolution,
+            surface_format,
+        }
+    }
+}
+
+pub fn cleanup_swapchain_components(
+    device: &ash::Device,
+    swapchain_loader: &swapchain::Device,
+    swapchain_components: &SwapchainComponents,
+) {
+    unsafe {
+        device.device_wait_idle().unwrap();
+        for &view in swapchain_components.present_image_views.iter() {
+            device.destroy_image_view(view, None);
+        }
+        swapchain_loader.destroy_swapchain(swapchain_components.swapchain, None)
+    };
+}
